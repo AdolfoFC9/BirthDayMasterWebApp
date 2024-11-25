123a94aff6ec8697b14f97c68ce7757397d98c78
Adolfo <afranco@mangochango.com>
2024-11-25 13:50:29 -0600

    Last Changes1

--- src/state/api.js
+++ src/state/api.js
@@ -18,76 +18,76 @@ export async function getPeople() {
     }
 }
 
-export async function getPeopleMonth(payload) { //espero llamdas exteras a getPeopleMonth y recibo los parametros que me envien el payload
+export async function getPeopleMonth(payload) { 
     try {
         const response = await axios({
             method: 'get',
-            url: `http://localhost:9000/people/${payload.params.month}`,  //agregamos a nuestra llamada al api el mes que recibimos en payload
+            url: `http://localhost:9000/people/${payload.params.month}`,  
             headers: {
                 "Content-Type": "application/json"
             }
-        });   //llamamos a nuestr API GET /people/:month enviandole como header content-type = application/jsonn y esperamos a que haya una respuesta
-        if (response.status === 200) { //validamos si el estatus de response y si es 200 devolvemos un objeto que contiene people >>>>  listado de personas que cumplen en dicho month
+        });   
+        if (response.status === 200) { 
             return { status: response.status, success: true, people: response.data };
         }
-        return { status: response.status, success: false, error: "Expected result was 200" };  // de lo contrario devolvemos el status que recibamos
+        return { status: response.status, success: false, error: "Expected result was 200" };  
     } catch (error) {
-        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  //  si algo no funciono dentro del try devolvemos normalmente 500 >>> Server Error
+        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  
     }
 }
 
-export async function insertPerson(payload) { //espero llamdas exteras a getPeopleMonth y recibo los parametros que me envien el payload
+export async function insertPerson(payload) { 
     try {
         const response = await axios({
             method: 'post',
-            url: `http://localhost:9000/person/`,  //agregamos a nuestra llamada al api el mes que recibimos en payload
+            url: `http://localhost:9000/person/`,  
             headers: {
                 "Content-Type": "application/json"
             },
             data: payload.person
-        });   //llamamos a nuestr API GET /people/:month enviandole como header content-type = application/jsonn y esperamos a que haya una respuesta
-        if (response.status === 200) { //validamos si el estatus de response y si es 200 devolvemos un objeto que contiene people >>>>  listado de personas que cumplen en dicho month
+        });   
+        if (response.status === 200) { 
             return { status: response.status, success: true, person: response.data };
         }
-        return { status: response.status, success: false, error: "Expected result was 200" };  // de lo contrario devolvemos el status que recibamos
+        return { status: response.status, success: false, error: "Expected result was 200" };  
     } catch (error) {
-        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  //  si algo no funciono dentro del try devolvemos normalmente 500 >>> Server Error
+        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  
     }
 }
 
-export async function updatePerson(payload) { //espero llamdas exteras a getPeopleMonth y recibo los parametros que me envien el payload
+export async function updatePerson(payload) { 
     try {
         const response = await axios({
             method: 'put',
-            url: `http://localhost:9000/person/${payload.person.identificador}`,  //agregamos a nuestra llamada al api el mes que recibimos en payload
+            url: `http://localhost:9000/person/${payload.person.identificador}`,  
             headers: {
                 "Content-Type": "application/json"
             },
             data: payload.person
-        });   //llamamos a nuestr API GET /people/:month enviandole como header content-type = application/jsonn y esperamos a que haya una respuesta
-        if (response.status === 200) { //validamos si el estatus de response y si es 200 devolvemos un objeto que contiene people >>>>  listado de personas que cumplen en dicho month
+        });   
+        if (response.status === 200) {
             return { status: response.status, success: true, person: response.data };
         }
-        return { status: response.status, success: false, error: "Expected result was 200" };  // de lo contrario devolvemos el status que recibamos
+        return { status: response.status, success: false, error: "Expected result was 200" };  
     } catch (error) {
-        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  //  si algo no funciono dentro del try devolvemos normalmente 500 >>> Server Error
+        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  
     }
 }
-export async function deletePerson(payload) { //espero llamdas exteras a getPeopleMonth y recibo los parametros que me envien el payload
+export async function deletePerson(payload) { 
 
     try {
         const response = await axios({
             method: 'delete',
-            url: `http://localhost:9000/person/${payload.identificador}`,  //agregamos a nuestra llamada al api el mes que recibimos en payload
+            url: `http://localhost:9000/person/${payload.identificador}`,  
             headers: {
                 "Content-Type": "application/json"
             }
-        });   //llamamos a nuestr API GET /people/:month enviandole como header content-type = application/jsonn y esperamos a que haya una respuesta
-        if (response.status === 204) { //validamos si el estatus de response y si es 200 devolvemos un objeto que contiene people >>>>  listado de personas que cumplen en dicho month
+        });   
+        if (response.status === 204) { 
             return { status: response.status, success: true, identificador: payload.identificador};
         }
-        return { status: response.status, success: false, error: "Expected result was 204" };  // de lo contrario devolvemos el status que recibamos
+        return { status: response.status, success: false, error: "Expected result was 204" };  
     } catch (error) {
-        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  //  si algo no funciono dentro del try devolvemos normalmente 500 >>> Server Error
+        return { status: error?.response?.status || 500, success: false, error: error.message, message: error?.response?.data?.message };  
     }
 }
